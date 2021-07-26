902bd855b44647cd35453bf838f89d49948ae5ed
HeMarchioni <henrique.marchioni@fatec.sp.gov.br>
2021-07-26 04:15:11 -0300

    initial commit

---
+++ src/main/java/com/dio/ControleAcesso/ControleAcessoApplication.java
@@ -0,0 +1,13 @@
+package com.dio.ControleAcesso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleAcessoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleAcessoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dio/ControleAcesso/ControleAcessoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dio.ControleAcesso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleAcessoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
